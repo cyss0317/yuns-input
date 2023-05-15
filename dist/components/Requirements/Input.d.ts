8d5cd267d33e6a40831011b1bda0b5caaccc9f63
Yun Choi <cyss0317@gmail.com>
2023-05-14 23:17:34 -0500

    Ran Build-lib for 1.1.2

--- dist/components/Requirements/Input.d.ts
+++ dist/components/Requirements/Input.d.ts
@@ -1,25 +1,25 @@
 import React from "react";
 import "./Input.css";
 interface InputProps {
-  name: string;
-  displayName: string;
-  fontSize?: FontSize;
-  color?: string;
-  type?: InputType;
-  value?: string;
-  onChange: (e: any) => void;
-  errorMessage?: string;
-  resetErrorMessage: () => void;
-  args?: any;
+    name: string;
+    displayName: string;
+    fontSize?: FontSize;
+    color?: string;
+    type?: InputType;
+    value?: string;
+    onChange: (e: any) => void;
+    errorMessage?: string | [string];
+    resetErrorMessage: () => void;
+    args?: any;
 }
 declare enum InputType {
-  TEXT = "text",
-  PASSWORD = "password",
+    TEXT = "text",
+    PASSWORD = "password"
 }
 declare enum FontSize {
-  "small" = "12px",
-  "medium" = "16px",
-  "large" = "20px",
+    "small" = "12px",
+    "medium" = "16px",
+    "large" = "20px"
 }
 export declare function Input(props: InputProps): React.JSX.Element;
 export {};
